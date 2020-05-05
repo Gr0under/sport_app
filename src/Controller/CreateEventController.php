5e4367818813dd6e7484e7f7d402f41e04c4473a
grounder <paul.juquelier@gmail.com>
2020-05-05 10:09:41 +0100

    ajout d'un formulaire sur plusieurs pages à l'aide des sessions

--- src/Controller/CreateEventController.php
+++ src/Controller/CreateEventController.php
@@ -6,26 +6,123 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\SportEvent;
-use App\Form\CreateEventType;
+use App\Form\CreateEventStep1Type;
+use App\Form\CreateEventStep2Type;
+use App\Form\CreateEventStep3Type;
 
 class CreateEventController extends AbstractController{
 
 	/**
 	 * @Route("/creer-un-evenement/{step}", name="createEvent")
 	 */
-	public function createEvent(EntityManagerInterface $em, $step )
+	public function createEvent(EntityManagerInterface $em, $step, Request $request, SessionInterface $session )
 	{
-		switch($step){
-			case "choisir-un-sport":
 
-				$createEventForm = $this->createform(CreateEventType::class);
+		$this->session = $session;
+		if($this->session->get("sportEvent") === null)
+		{
+			$sportEvent = new SportEvent();
+			$this->session->set('sportEvent', $sportEvent);
+			echo "session créée";
+		}
+
+		switch($step){
+
+			case "choisir-un-sport":
+				
+
+				$formStep1 = $this->createform(CreateEventStep1Type::class);
+				$formStep1->handleRequest($request); 
+
+				if($formStep1->isSubmitted() && $formStep1->isValid())
+				{
+					$data = $formStep1->getData();
+
+
+					$this->session->get("sportEvent")->setTitle($data->getTitle());
+					dump($this->session->get("sportEvent"));
+					
+					return $this->redirectToRoute("createEvent", ["step"=>"description"]);
+				}
+
 				return $this->render("createEventForm/step1.html.twig", [
-					"createEventForm" => $createEventForm->createView(),
+					"createEventForm" => $formStep1->createView(),
 				]);
 
 			break;
+
+			case "description":
+
+				$formStep2 = $this->createform(CreateEventStep2Type::class);
+				$formStep2->handleRequest($request); 
+
+				if($formStep2->isSubmitted() && $formStep2->isValid()){
+					$data = $formStep2->getData();
+					$this->session->get("sportEvent")->setDescription($data->getDescription());  
+					return $this->redirectToRoute("createEvent", ["step"=>"tout-le-reste"]);
+				}
+
+				return $this->render("createEventForm/step2.html.twig", [
+					"createEventForm" => $formStep2->createView(),
+				]);
+
+			break; 
+
+			case "tout-le-reste": 
+				$formStep3 = $this->createform(CreateEventStep3Type::class);
+				$formStep3->handleRequest($request); 
+
+				if($formStep3->isSubmitted() && $formStep3->isValid()){
+
+					$data = $formStep3->getData();
+					$this->session->get('sportEvent')
+						->setOrganiser($data->getOrganiser())
+						->setlocationDpt($data->getlocationDpt())
+						->setlocationCity($data->getlocationCity())
+						->setLocationAddress($data->getLocationAddress())
+						->setThumbnail($data->getThumbnail())
+						->setPlayer($data->getPlayer())
+						->setLevel($data->getLevel())
+						->setLevelDescription($data->getLevelDescription())
+						->setMaterial($data->getMaterial())
+						->setAssemblyPoint($data->getAssemblyPoint())
+						->setPriceDescription($data->getPriceDescription())
+						->setDistance($data->getDistance())
+						->setPace($data->getPace())
+						->setCreatedAt($data->getCreatedAt())
+						->setUpdatedAt($data->getUpdatedAt())
+						->setDate($data->getDate())
+						->setTimeStart($data->getTimeStart())
+						->setTimeEnd($data->getTimeEnd());
+					
+
+					dump($this->session->get('sportEvent'));
+
+					if(null !== $this->session->get('sportEvent'))
+					{
+						$em->persist($this->session->get('sportEvent')); 
+						$em->flush();
+					}else{
+						echo "Aucun objet à envoyer en DB"; 
+					}
+
+					$this->session->remove('sportEvent');  
+					dump($this->session->get('sportEvent'));
+
+					 die();
+					return $this->redirectToRoute("home");
+				}
+
+
+				return $this->render("createEventForm/step3.html.twig", [
+					"createEventForm" => $formStep3->createView(),
+				]);
+			break;
+
+
 		}
 
 	
---
+++ src/Form/CreateEventStep3Type.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\SportEvent;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateEventStep3Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+           ->add('organiser')
+           ->add('location_dpt')
+           ->add('location_city')
+           ->add('location_address')
+           ->add('thumbnail')
+           ->add('player')
+           ->add('level')
+           ->add('level_description')
+           ->add('material')
+           ->add('assembly_point')
+           ->add('price_description')
+           ->add('distance')
+           ->add('pace')
+           ->add('created_at')
+           ->add('updated_at')
+           ->add('date')
+           ->add('time_start')
+           ->add('time_end')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => SportEvent::class,
+        ]);
+    }
+}
